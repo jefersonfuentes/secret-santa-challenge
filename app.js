f4df79b5d3a5c189d089e1cd6be5a43126b120fa
Jeferson Fuentes <jeferson.fg710@gmail.com>
2025-01-28 00:23:13 -0600

    Function to display the friend list implemented

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 let amigos = [];
+let lista = document.getElementById('listaAmigos');
 function agregarAmigo() {
-    let nombre = document.getElementById('amigo').value;
-    if(nombre == '') {
+    let nombre = document.getElementById('amigo').value
+    if(nombre == ''){
         alert('Por favor, inserte un nombre');
     } else if (amigos.includes(nombre)) {
-        alert('Error, este nombre ya esta en la lista');
+        alert('Error este nombre ya está en la lista')
     } else {
         amigos.push(nombre);
-        console.log(amigos);
         document.getElementById('amigo').value = '';
+        actualizarAmigos();
     }
 }
 
+function actualizarAmigos() {
+    lista.innerHTML = ""
+    
+    for(let i = 0; i < amigos.length; i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
